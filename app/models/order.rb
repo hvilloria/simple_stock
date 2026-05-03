33e1150677bf220e28462930ef274f617dd47c84
Hosward Villoria <hosward.avc@gmail.com>
2026-05-03 18:21:43 -0300

    feat (feat_04): declare has_many :payments on Order
    Used by Sales::CancelOrder to destroy associated payments and by
    Sales::CreateOrder for the inverse association of the new initial
    payment relation.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
   has_many :stock_movements, as: :reference, dependent: :nullify
+  has_many :payments, dependent: :destroy
 
   # Nested attributes
   accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: :all_blank
